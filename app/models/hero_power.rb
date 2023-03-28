f6c7ffbab303906e0ab99ce13295116bc9037256
Brian Wahungu <cepteinhawks@gmail.com>
2023-03-28 13:23:23 +0300

    added all route files

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,5 @@
 class HeroPower < ApplicationRecord
   belongs_to :superhero
   belongs_to :power
+  validates :strength, inclusion: { in: ['Strong', 'Weak', 'Average'] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
   resources :hero_powers
   resources :superheros
   post '/superheros', to: 'superheros#create'
+
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
+
+  # Handle not found routes
+  get '*unmatched_route', to: 'application#not_found'
+
+  get '/heroes', to: 'heroes#index'
+  get '/heroes/:id', to: 'heroes#show'
+  get '/powers/:id', to: 'powers#show'
+  patch '/powers/:id', to: 'powers#update'
+  post '/hero_powers', to: 'hero_powers#create'
+
+
 end
